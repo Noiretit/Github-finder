90afeb367e5f6ddaa4f0b1b86f25797645a0a7f7
Noiret <sergio.ko@icloud.com>
2020-09-22 12:20:27 +0200

    Installed axios to fetch from Github. App.js: async await on componentDidMount, change loading state from true to false to show spinner while fetching, passes props to User component. Users component: deleted hardcoded state, pass props to UserItem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navbar from './components/layout/Navbar'
-import User from './components/users/Users'
+import axios from 'axios';
+import Navbar from './components/layout/Navbar';
+import User from './components/users/Users';
 
 class App extends Component {
+
+  state = {
+    users: [],
+    loading: false
+  }
+
+  //When component is mounting (data is being fetched) we change "loading" to
+  //false so the spinner is showing 
+  async componentDidMount(){
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users')
+    
+    this.setState({ users: res.data, loading: false})
+  }
 
   render() {
   
@@ -11,7 +27,7 @@ class App extends Component {
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github"/>
         <div className='container'>
-          <User/>
+          <User loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
     );
